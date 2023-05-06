802afa04cc3c23a28afff9e07aa0954177f4de11
Marcelo Henrique da Silva <marcelo@pop-os.localdomain>
2023-05-05 23:37:02 -0300

    Alterando page content para deixar dinâmico

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,15 +2,15 @@ import { identifierName } from "@angular/compiler";
 
 export const dataFake = [
   {
-    "id": 1,
-    "photo": "aaaa",
-    "title": "novo Filme",
-    "description": "Ola"
+    "id": "1",
+    "photoCover": "https://www.alura.com.br/artigos/assets/como-comecar-com-angular/como-comecar-com-angular.png",
+    "title": "Nova versão angular",
+    "description": "Novas funcionalidades vem ai"
   },
   {
-    "id": 2,
-    "photo": "aaaea",
-    "title": "nova série",
-    "description": "Olaaaaa"
+    "id": "2",
+    "photoCover": "https://muralgamer.com/wp-content/uploads/2023/05/Filme-Gran-Turismo-01.jpg",
+    "title": "Gran Turismo Vem Ai",
+    "description": "Novo novo chega as telinhas em agosto"
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Action } from 'rxjs/internal/scheduler/Action';
+import {dataFake} from 'src/app/data/dataFake';
+
 
 @Component({
   selector: 'app-content',
@@ -8,9 +9,10 @@ import { Action } from 'rxjs/internal/scheduler/Action';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  photoCover: string = "https://www.alura.com.br/artigos/assets/como-comecar-com-angular/como-comecar-com-angular.png";
-  contentTitle: string = "MINHA NOTICIA";
-  contentDescription: string = "OLá mundo";
+  photoCover: string = "";
+  contentTitle: string = "";
+  contentDescription: string = "";
+  private id: string | null = "0"
 
   constructor(
     private route:ActivatedRoute
@@ -18,8 +20,20 @@ export class ContentComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+      this.id = value.get("id")
     )
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id: string | null){
+    const result = dataFake.filter(
+                      article => article.id == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+
+
   }
 
 }
